0e1097d0982b241f999b07fd35f1f8191436652e
Roshan Veervani <109918610+Roshan-Veervani@users.noreply.github.com>
2024-01-26 18:43:40 +0530

    Add files via upload

---
+++ SleepTracker/app/src/main/java/com/example/sleeptracker/RecordSleepActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sleeptracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordSleepActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_sleep)
+    }
+}
